c32780090cbcf0d5404fbcee22e06549033e9dfb
git.sujalsingh@gmail.com <git.sujalsingh@gmail.com>
2026-05-31 11:17:12 +0530

    Updated DailyPlanner

--- script.js
+++ script.js
@@ -14,27 +14,41 @@ function opencard() {
     })
 }
 opencard()
- 
+
+function todolist(){
+     
 let currenttask = []
  
 if(localStorage.getItem('currenttask')){
     currenttask = JSON.parse(localStorage.getItem('currenttask'))
-    console.log('task is full')
-}else{
-    console.log('task is empty')
-}
+} 
 
 function rendertask() {
     let addtask = document.querySelector('.addtask')
     let sum = ''
-    currenttask.forEach(function (elem) {
+    currenttask.forEach(function (elem,idx) {
         sum = sum + `<div class="tasks">
-            <h5>${elem.task}<span class = "${elem.imp}">imp</span></h5>
-            <button>Mark as Completed</button>
+            <h5>${elem.task}<span class = "${elem.imp}">imp</span>
+            <details>
+            <summary></summary>
+            <p>${elem.detail}</p>
+            </details> 
+            </h5>
+            <button id = ${idx}>Mark as Completed</button>
             </div>`
     })
 
     addtask.innerHTML = sum
+ localStorage.setItem('currenttask',JSON.stringify(currenttask))
+
+    let Cmpltbtn = document.querySelectorAll(".tasks button")
+
+Cmpltbtn.forEach(function (btn){
+    btn.addEventListener("click",function(){
+        currenttask.splice(btn.id,1)
+        rendertask()
+    })    
+}) 
 }
 rendertask()
 
@@ -45,7 +59,7 @@ let formcheckbox = document.querySelector("#checkbox")
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
-    if (forminput.value == '' && formdetail.value == '') {
+    if (forminput.value == '' || formdetail.value == '') {
         return
     }
     currenttask.push(
@@ -55,10 +69,41 @@ form.addEventListener('submit', function (e) {
             imp: formcheckbox.checked
         }
     )
-     localStorage.setItem('currenttask',JSON.stringify(currenttask))
+    
     forminput.value = '',
     formdetail.value = '',
     formcheckbox.checked = false
     rendertask()
 })
+}
+todolist()
 
+function dailyplan()
+{
+    let dayplanData = JSON.parse(localStorage.getItem('dayplanData'))|| {}
+
+
+var hour = Array.from({length:18},function(_,idx){
+    return  `${6+idx}:00 - ${7+idx}:00`
+})
+
+let dailyplan = document.querySelector(".dailyplan")
+var wholedaysum = ''
+hour.forEach(function(elem,idx){
+   var savedata = dayplanData[idx] || ''
+    wholedaysum = wholedaysum + ` <div class="dailytime">
+                <p class="time">${elem}</p>
+                 <input id = ${idx} type="text" placeholder = "..." value = "${savedata} "autocomplete = "off">
+              </div>`
+})
+dailyplan.innerHTML = wholedaysum
+
+var dailyplanInput = document.querySelectorAll(".dailyplan input")
+dailyplanInput.forEach(function(elem){
+       elem.addEventListener('input',function(){
+            dayplanData[elem.id] = elem.value        
+            localStorage.setItem('dayplanData',JSON.stringify(dayplanData)) 
+       })
+})
+}
+dailyplan()
